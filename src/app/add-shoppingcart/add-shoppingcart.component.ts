ce45740c055aa838cf2613ca83aa9b07858c4825
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-25 14:26:33 +0530

    add cart

---
+++ src/app/add-shoppingcart/add-shoppingcart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-shoppingcart',
+  templateUrl: './add-shoppingcart.component.html',
+  styleUrls: ['./add-shoppingcart.component.css']
+})
+export class AddShoppingcartComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddShoppingcartComponent } from './add-shoppingcart/add-shoppingcart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddShoppingcartComponent
   ],
   imports: [
     BrowserModule,
